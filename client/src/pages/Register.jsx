0db5ac642d73740451548955a31d885dd6e5aef7
Borys Mch <borys.mch@gmail.com>
2023-01-24 08:09:00 +0200

    Created Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center">Login</h1>
+        <h1 className="text-4xl text-center">Register</h1>
         <form className="max-w-md mx-auto">
+          <input type="text" placeholder="John Doe" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
-            Don't have an account yet?
-            <Link className="underline text-black" to={"/register"}>
-              Register
+            Already a member?
+            <Link className="underline text-black" to={"/login"}>
+              Login
             </Link>
           </div>
         </form>
