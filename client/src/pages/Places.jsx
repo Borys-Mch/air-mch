5357084aa766e989bb164e32ce2477bc3c9ad87c
Borys Mch <borys.mch@gmail.com>
2023-01-26 13:13:04 +0200

    Added checkbox perks on  Places page

--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import IconsReactjs from "icons-reactjs";
 
 export default function Places() {
   const { action } = useParams();
@@ -39,15 +40,85 @@ export default function Places() {
               type="text"
               placeholder="Title, for example:  My lovely apt"
             />
+
             <h2 className="text-2xl mt-4">Address</h2>
             <p className="text-gray-500 text-sm">Address to this place</p>
             <input type="text" placeholder="Address" />
+
             <h2 className="text-2xl mt-4">Photos</h2>
             <p className="text-gray-500 text-sm">More = better</p>
-            <div className="mt-3 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-                +
+            <div className="flex gap-2">
+              <input type="text" placeholder={"Add using a link ...jpg"} />
+              <button className="bg-gray-200 px-4 rounded-2xl">
+                Add&nbsp;photo
               </button>
+            </div>
+            <div className="mt-3 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                <IconsReactjs
+                  icon={"upload-cloud-outline"}
+                  fontSize={"1.8rem"}
+                  color={"#4b5563"}
+                />
+                Upload
+              </button>
+            </div>
+
+            <h2 className="text-2xl mt-4">Description</h2>
+            <p className="text-gray-500 text-sm">Description of the place</p>
+            <textarea />
+
+            <h2 className="text-2xl mt-4">Perks</h2>
+            <p className="text-gray-500 text-sm">
+              Select all the perks of your place
+            </p>
+            <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs
+                  icon={"wifi"}
+                  fontSize={"1.2rem"}
+                  color={"#000"}
+                />
+                <span>WIFI</span>
+              </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs icon={"cab"} fontSize={"1.2rem"} color={"#000"} />
+                <span>Free parking spot</span>
+              </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs
+                  icon={"food-1"}
+                  fontSize={"1.2rem"}
+                  color={"#000"}
+                />
+                <span>Free denner</span>
+              </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs icon={"tv"} fontSize={"1.2rem"} color={"#000"} />
+                <span>TV</span>
+              </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs
+                  icon={"guidedog"}
+                  fontSize={"1.2rem"}
+                  color={"#000"}
+                />
+                <span>Pets</span>
+              </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <IconsReactjs
+                  icon={"key-outline"}
+                  fontSize={"1.2rem"}
+                  color={"#000"}
+                />
+                <span>Private enterance</span>
+              </label>
             </div>
           </form>
         </div>
